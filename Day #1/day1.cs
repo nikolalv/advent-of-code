fdce87ca6c036086516864a52a2db1e60c5e4aee
Nik <nikola.luettgen.vear@gmail.com>
2019-12-01 20:30:43 -0500

    Day #1 - Part 1 Short
    I was requested to make my code shorter.

--- Day #1/day1.cs
+++ Day #1/day1.cs
@@ -1,13 +1,7 @@
-        // Will require System.IO
+        // Will require System.IO and System.Linq
         static int GetFuel(double x) => Convert.ToInt32(Math.Floor(x / 3)) - 2;
 
-        static int Part1()
-        {
-            int sum = 0;
-            foreach (string line in File.ReadAllLines("input.txt"))
-                sum += GetFuel(double.Parse(line));
-            return sum;
-        }
+        static int Part1() => File.ReadAllLines("input.txt").Select(x => GetFuel(double.Parse(x))).Sum();
 
         static int Part2()
         {
